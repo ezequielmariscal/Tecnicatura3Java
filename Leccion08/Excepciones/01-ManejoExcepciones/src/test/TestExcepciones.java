0717be56e225e985520ef42d8babb502fb8a122e
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-06-07 22:48:02 -0300

    Add_class8_part9

--- Leccion08/Excepciones/01-ManejoExcepciones/src/test/TestExcepciones.java
+++ Leccion08/Excepciones/01-ManejoExcepciones/src/test/TestExcepciones.java
@@ -1,10 +1,12 @@
 package test;
 
+import aritmetica.Aritmetica;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
         try{
-            resultado = 10/0;
+            resultado = Aritmetica.division(10, 0);
         }catch(Exception e){
             System.out.println("Ocurrio un Error");
             e.printStackTrace(System.out); // Se conoce como la pila de excepciones
