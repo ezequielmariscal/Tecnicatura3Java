c3189295adec2a2c27e7d854ea7c793f560a8739
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-06-09 01:54:45 -0300

    Add_cal_part4

--- Leccion09/01-CalculadoraUTN/src/CalculadoraUTN.java
+++ Leccion09/01-CalculadoraUTN/src/CalculadoraUTN.java
@@ -4,11 +4,24 @@ public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("******* Aplicacion Calculadora *******");
-        System.out.print("Digite el valor para el operando 1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite el valor para el operando 2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
+        // Mostramos el menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.println("Operacion a realizar?");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if (operacion >= 1 && operacion <= 4){
+            System.out.print("Digite el valor para el operando 1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Digite el valor para el operando 2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+
+        }
+
     }
 }
