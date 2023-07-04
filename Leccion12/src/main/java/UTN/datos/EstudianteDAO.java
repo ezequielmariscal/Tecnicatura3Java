cedadaf69adcdf2aad7d1248bc692a8d28867c46
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-07-04 18:36:24 -0300

    Add_class12_part8

--- Leccion12/src/main/java/UTN/datos/EstudianteDAO.java
+++ Leccion12/src/main/java/UTN/datos/EstudianteDAO.java
@@ -83,5 +83,15 @@ public class EstudianteDAO {
         List<Estudiante> estudiantes = estudianteDAO.listarEstudiantes();
         estudiantes.forEach(System.out::println); // Funcion lambda para imprimir
 
+        // Buscar por id
+        var estudiante1 =  new Estudiante(1);
+        System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
+        var encontrado = estudianteDAO.buscarEstudiantePorId(estudiante1);
+        if(encontrado)
+            System.out.println("Estudiante encontrado: "+estudiante1);
+        else
+            System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante());
+
+
     }
 }
