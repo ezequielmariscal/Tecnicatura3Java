5ec7117608499d1c24f40a6150488cf066119f39
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-07-04 17:27:00 -0300

    Add_class12_parte3

--- Leccion12/src/main/java/UTN/datos/EstudianteDAO.java
+++ Leccion12/src/main/java/UTN/datos/EstudianteDAO.java
@@ -2,6 +2,8 @@ package UTN.datos;
 
 import UTN.dominio.Estudiante;
 
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -13,6 +15,10 @@ public class EstudianteDAO {
         // Creamos algunos objetos que son necesarios para comunicarnos con la bd
         PreparedStatement ps; // Envia la sentencia a la base de datos
         ResultSet rs; // Obtenemos el resultado de la bd
+        // Creamos un objeto de tipo conexion
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022";
+
 
 
     }
